b329a2585fc88c059879f0fe205a67ab0a036fed
Josh Latour <jlatour88@gmail.com>
2019-11-25 14:44:47 -0600

    Testing camera functionality

--- client/src/pages/AddInventory/AddInvForm/AddInvForm.js
+++ client/src/pages/AddInventory/AddInvForm/AddInvForm.js
@@ -1,83 +1,96 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Field } from 'react-final-form'
+import MyCamera from '../../../components/MyCamera/MyCamera';
 
 import styles from './AddInvForm.module.css';
 
-const AddInvForm = (props) => (
-  <Form
-    onSubmit={props.onSubmit}
-    render={({ handleSubmit, form, submitting, pristine, values }) => (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label className={styles.inputHeader}>Material</label>
-          <Field
-            name='material'
-            component='input'
-            type='text'
-            placeholder='Blue Fire'
-          />
-        </div>
-        <div>
-          <label className={styles.inputHeader}>Material Type</label>
-          <Field name='type' component='select'>
-            <option value=''>Choose Type</option>
-            <option value='marble'>Marble</option>
-            <option value='granite'>Granite</option>
-            <option value='quartz'>Quartz</option>
-          </Field>
-        </div>
-        <div>
-          <label className={styles.inputHeader}>Material Finish</label>
-          <label className={styles.checkboxLabel}>
-            <Field
-              name="finish"
-              component="input"
-              type="checkbox"
-              value="honed"
-            />{' '}
-            Honed
+const AddInvForm = (props) => {
+  // const [camera, toggleCamera] = useState(false);
+  // const toggleCameraHandler = () => {
+  //   toggleCamera(!camera)
+  // }
+  return (
+    <>
+      <Form
+        onSubmit={props.onSubmit}
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label className={styles.inputHeader}>Material</label>
+              <Field
+                name='material'
+                component='input'
+                type='text'
+                placeholder='Blue Fire'
+              />
+            </div>
+            <div>
+              <label className={styles.inputHeader}>Material Type</label>
+              <Field name='type' component='select'>
+                <option value=''>Choose Type</option>
+                <option value='marble'>Marble</option>
+                <option value='granite'>Granite</option>
+                <option value='quartz'>Quartz</option>
+              </Field>
+            </div>
+            <div>
+              <label className={styles.inputHeader}>Material Finish</label>
+              <label className={styles.checkboxLabel}>
+                <Field
+                  name="finish"
+                  component="input"
+                  type="checkbox"
+                  value="honed"
+                />{' '}
+                Honed
           </label>
-          <label className={styles.checkboxLabel}>
-            <Field
-              name="finish"
-              component="input"
-              type="checkbox"
-              value="polished"
-            />{' '}
-            Polished
+              <label className={styles.checkboxLabel}>
+                <Field
+                  name="finish"
+                  component="input"
+                  type="checkbox"
+                  value="polished"
+                />{' '}
+                Polished
           </label>
-          <label className={styles.checkboxLabel}>
-            <Field
-              name="finish"
-              component="input"
-              type="checkbox"
-              value="leathered"
-            />{' '}
-            Leathered
+              <label className={styles.checkboxLabel}>
+                <Field
+                  name="finish"
+                  component="input"
+                  type="checkbox"
+                  value="leathered"
+                />{' '}
+                Leathered
           </label>
-        </div>
-        <div>
-          <label className={styles.inputHeader}>Material Size</label>
-          <Field
-            name='size'
-            component='input'
-            type='text'
-            placeholder='170 x 85'
-          />
-        </div>
+            </div>
+            <div>
+              <label className={styles.inputHeader}>Material Size</label>
+              <Field
+                name='size'
+                component='input'
+                type='text'
+                placeholder='170 x 85'
+              />
+            </div>
 
-        <div className='buttons'>
-          <button type='submit' disabled={submitting || pristine}>
-            Submit
+            <div className='buttons'>
+              <button type='submit' disabled={submitting || pristine}>
+                Submit
             </button>
-          <button type='button' onClick={form.reset} disabled={submitting || pristine}>
-            Reset
+              <button type='button' onClick={form.reset} disabled={submitting || pristine}>
+                Reset
             </button>
-        </div>
-        <pre>{JSON.stringify(values, 0, 2)}</pre>
-      </form>
-    )}
-  />
-)
+            </div>
+            {/* <button onClick={toggleCameraHandler}>toggle camera</button>
+            {camera && <MyCamera material={values.name} />} */}
+            <MyCamera material={values.material} />
+
+            <pre>{JSON.stringify(values, 0, 2)}</pre>
+          </form>
+        )}
+      />
+    </>
+  )
+}
 
 export default AddInvForm;
