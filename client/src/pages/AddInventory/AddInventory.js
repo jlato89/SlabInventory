010977d28bd77e3b227a99fa62f48850dbd43903
Josh Latour <jlatour88@gmail.com>
2019-11-29 16:01:19 -0600

    pass formObj to server

--- client/src/pages/AddInventory/AddInventory.js
+++ client/src/pages/AddInventory/AddInventory.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import AddInvForm from './AddInvForm/AddInvForm';
+import axios from 'axios';
 
 class AddInventory extends Component {
   constructor(props) {
@@ -10,6 +11,12 @@ class AddInventory extends Component {
   onSubmit = (formObj) => {
     formObj.material.finish = formObj.material.finish.toString();
     console.log('[AddInventory]', formObj);
+
+    axios.post('/api/addMaterial', formObj)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => console.log(err));
   };
 
   render() {
