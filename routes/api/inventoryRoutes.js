2a71f6e36e7b8593c8b72e0f892a6207d2c18d12
Josh Latour <jlatour88@gmail.com>
2019-11-30 00:27:35 -0600

    testing various ways of handling FormData obj from form

--- routes/api/inventoryRoutes.js
+++ routes/api/inventoryRoutes.js
@@ -1,3 +1,49 @@
-module.exports = app => {
+const db = require('../../models');
+const formidable = require('formidable');
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploadTesting/')
+  },
+  filename: (req, file, cb) => {
+    const fm = file.mimetype;
+    const imgType = fm.substr(fm.indexOf('/') + 1);
+    cb(null, file.fieldname + '.' + imgType)
+  }
+})
+const upload = multer({ storage: storage })
 
+module.exports = app => {
+  app.post('/api/addMaterial',
+    upload.any(),
+    (req, res, next) => {
+      console.log(req.body);
+      console.log(req.files);
+    })
+
+
+  // app.post('/api/addMaterial', (req, res) => {
+  //   new formidable.IncomingForm().parse(req)
+  //     .on('field', (name, field) => {
+  //       console.log('Field', name, field)
+  //     })
+  //     .on('fileBegin', (name, file) => {
+  //       file.name = name + '.png'; //! temp fix, change to grab char after '/'
+  //       file.path = 'uploadTesting/' + file.name
+  //     })
+  //     .on('file', (name, file) => {
+  //       console.log(`Uploaded file ${name} to ${file.path}`)
+  //     })
+  //     .on('aborted', () => {
+  //       console.error('Request aborted by the user')
+  //     })
+  //     .on('error', (err) => {
+  //       console.error('Error', err)
+  //       throw err
+  //     })
+  //     .on('end', () => {
+  //       res.end()
+  //     })
+  //   res.status(200);
+  // });
 };
