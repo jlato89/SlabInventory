c58400386663af73b1f4f6d36efabb48a4c230bc
Josh Latour <jlatour88@gmail.com>
2019-11-30 02:42:49 -0600

    forgot to save, check last commit

--- routes/api/inventoryRoutes.js
+++ routes/api/inventoryRoutes.js
@@ -1,5 +1,4 @@
 const db = require('../../models');
-const formidable = require('formidable');
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => { cb(null, 'uploadTesting/') },
@@ -14,30 +13,4 @@ module.exports = app => {
       console.log('Fields', req.body);
       console.log('Files', req.files);
     })
-
-
-  // app.post('/api/addMaterial', (req, res) => {
-  //   new formidable.IncomingForm().parse(req)
-  //     .on('field', (name, field) => {
-  //       console.log('Field', name, field)
-  //     })
-  //     .on('fileBegin', (name, file) => {
-  //       file.name = name + '.png'; //! temp fix, change to grab char after '/'
-  //       file.path = 'uploadTesting/' + file.name
-  //     })
-  //     .on('file', (name, file) => {
-  //       console.log(`Uploaded file ${name} to ${file.path}`)
-  //     })
-  //     .on('aborted', () => {
-  //       console.error('Request aborted by the user')
-  //     })
-  //     .on('error', (err) => {
-  //       console.error('Error', err)
-  //       throw err
-  //     })
-  //     .on('end', () => {
-  //       res.end()
-  //     })
-  //   res.status(200);
-  // });
 };
