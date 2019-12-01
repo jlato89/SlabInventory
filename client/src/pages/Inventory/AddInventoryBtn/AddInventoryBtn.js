dd432b93e44c88a9cec3b927bb8f71ce214394ba
Josh Latour <jlatour88@gmail.com>
2019-12-01 15:07:25 -0600

    added link to add-inventory page

--- client/src/pages/Inventory/AddInventoryBtn/AddInventoryBtn.js
+++ client/src/pages/Inventory/AddInventoryBtn/AddInventoryBtn.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 import './AddInventoryBtn.module.css';
 
 const AddInventoryBtn = (props) => {
   return (
     <footer>
-      <Button
-        block
-        size='lg'
-        onClick={props.onClickHandler}
-      >
-        Add Inventory
-      </Button>
+      <Link to='/add-inventory'>
+        <Button
+          block
+          size='lg'
+          onClick={props.onClickHandler}
+        >
+          Add Inventory
+        </Button>
+      </Link>
     </footer>
   )
 }
