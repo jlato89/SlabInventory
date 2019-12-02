50724706ea70303511eb8fc0c0df3b1f02c91180
Josh Latour <jlatour88@gmail.com>
2019-12-02 15:11:37 -0600

    setting img path, converting imgString back to array to loop thru

--- client/src/pages/Inventory/SlabModal/ImageCarousel/ImageCarousel.js
+++ client/src/pages/Inventory/SlabModal/ImageCarousel/ImageCarousel.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 const ImageCarousel = (props) => {
+  const path = '/assets/images/';
+  const imgArr = props.images.split(',');
+  console.log(imgArr);
   return (
     <Carousel style={{ marginBottom: '15px' }} interval={null}>
-      {props.images.map((image, index) => (
+      {imgArr.map((image, index) => (
         <Carousel.Item key={index}>
           <img
             className='d-block w-100'
-            src={image}
+            src={path + image}
             alt={'Slab Image ' + index}
           />
         </Carousel.Item>
